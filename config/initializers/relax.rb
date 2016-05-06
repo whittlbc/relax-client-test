6e1a7fc9d15cfd5714d11ad650b10936f8be919b
Ben Whittle <benwhittle31@gmail.com>
2016-05-06 10:29:48 -0700

    changing log statement again

--- config/initializers/relax.rb
+++ config/initializers/relax.rb
@@ -1,7 +1,5 @@
 callback = Proc.new do |event|
-  Rails.logger.info "received event: #{event}"
-  puts "yayyyy"
-  # handle event
+  puts "received event: #{event}"
 end
 
 Relax::EventListener.callback = callback
